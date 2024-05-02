331f13a1d474860a7006f6eee25f97cdb3779884
Monster dragon <165640633+Sumitmeena12@users.noreply.github.com>
2024-05-03 00:32:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -78,10 +78,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/0fc7723820d9b4e397de5.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/195c462935d1c72b74724.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://graph.org//file/d924fadcdc8e03a425950.jpg"
+    "PING_IMG_URL", "https://telegra.ph/file/195c462935d1c72b74724.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
